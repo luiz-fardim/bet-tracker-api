b69f7186d4121bf5cea8c817b3e14742567e0903
Luiz <luikznn364@gmail.com>
2026-06-22 17:41:47 -0300

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { BetsModule } from './bets/bets.module';
+
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    BetsModule,
+    ConfigModule.forRoot({ isGlobal: true}),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get('DB_HOST'),
+        port: +config.get('DB_PORT'),
+        username: config.get('DB_USER'),
+        password: config.get('DB_PASSWORD'),
+        database: config.get('DB_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+      }),
+      inject: [ConfigService]
+    })
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
