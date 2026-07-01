b93f0ab58f52b4dd55d299a2ed7d7044e8e6f2c9
Luiz <luizfernandofardim@gmail.com>
2026-07-01 16:38:24 -0300

    atualizando a rota inicial

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Seja Bem-Vindo ao Bet-Tracker, o melhor gerenciador de apostas que você já viu!';
   }
 }
