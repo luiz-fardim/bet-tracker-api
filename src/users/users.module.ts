09597e363254c7c29522ee9184acbe4e7f774402
Luiz <luizfernandofardim@gmail.com>
2026-07-04 22:54:31 -0300

    consertando bug no modules

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,8 +3,6 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
-import { APP_GUARD } from '@nestjs/core';
-import { RolesGuard } from 'src/guards/authorization/roles.guard';
 import { JwtService } from '@nestjs/jwt';
 
 @Module({
@@ -12,11 +10,8 @@ import { JwtService } from '@nestjs/jwt';
   providers: [
     UsersService,
     JwtService,
-    {
-      provide: APP_GUARD,
-      useClass: RolesGuard
-    }
   ],
   imports: [TypeOrmModule.forFeature([User])],
 })
 export class UsersModule { }
+  
