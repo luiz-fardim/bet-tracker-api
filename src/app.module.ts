6e307a37aac6b75f5411568569fd3836967c1c32
Luiz <luizfernandofardim@gmail.com>
2026-07-07 20:39:42 -0300

    apagando app.service e app.controller do app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { BetsModule } from './bets/bets.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
@@ -23,8 +21,6 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
     BetsModule,
     UsersModule,
     AuthModule,
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+  ]
 })
 export class AppModule {}
