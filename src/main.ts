d4cc00aade79f08fcdd0942ab3a3150d79c43d9e
Luiz <luizfernandofardim@gmail.com>
2026-07-13 17:31:25 -0300

    add cors

--- src/main.ts
+++ src/main.ts
@@ -7,27 +7,32 @@ import helmet from 'helmet';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
+  })
   app.use(helmet());
-
   app.enableVersioning({
     type: VersioningType.URI,
   });
-
+  
   const config = new DocumentBuilder()
-    .setTitle('Bet Tracker API')
-    .setDescription(
-      'A REST API for managing, recording, and analyzing sports bets. Register your bets, track results, and monitor your profit/loss automatically.  ',
-    )
-    .setVersion('1.0')
-    .build();
+  .setTitle('Bet Tracker API')
+  .setDescription(
+    'A REST API for managing, recording, and analyzing sports bets. Register your bets, track results, and monitor your profit/loss automatically.  ',
+  )
+  .setVersion('1.0')
+  .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
-
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       transform: true,
     }),
+
   );
   await app.listen(process.env.PORT ?? 3000);
 }
